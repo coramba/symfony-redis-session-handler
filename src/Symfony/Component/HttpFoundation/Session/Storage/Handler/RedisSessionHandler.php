4d6a18e6f9c217271f3ad8ff1cd388984de52c87
coramba <coramba@yandex.ru>
2020-01-21 01:20:00 +0300

    Replace deprecated method

--- src/Symfony/Component/HttpFoundation/Session/Storage/Handler/RedisSessionHandler.php
+++ src/Symfony/Component/HttpFoundation/Session/Storage/Handler/RedisSessionHandler.php
@@ -11,120 +11,124 @@
 
 namespace Symfony\Component\HttpFoundation\Session\Storage\Handler;
 
+use InvalidArgumentException;
+use Redis;
+use SessionHandlerInterface;
+
 /**
  * RedisSessionHandler
  *
  * @author Markus Bachmann <markus.bachmann@bachi.biz>
  * @author Ryan Schumacher <ryan@38pages.com>
  */
-class RedisSessionHandler implements \SessionHandlerInterface
-{
-    /**
-     * @var \Redis
-     */
-    private $redis;
+class RedisSessionHandler implements SessionHandlerInterface {
+	/**
+	 * @var Redis
+	 */
+	private $redis;
 
-    /**
-     * @var integer
-     */
-    private $lifetime;
+	/**
+	 * @var integer
+	 */
+	private $lifetime;
 
-    /**
-     * @var array
-     */
-    private $options;
+	/**
+	 * @var array
+	 */
+	private $options;
 
-    /**
-     * Constructor
-     * 
-     * List of available options:
-     *  * key_prefix: The key prefix [default: '']
-     *
-     * @param \Redis $redis The redis instance
-     * @param integer $lifetime Max lifetime in seconds to keep sessions stored.
-     * @param array $options Options for the session handler
-     * 
-     * @throws \InvalidArgumentException When Redis instance not provided
-     */
-    public function __construct($redis, $lifetime, array $options = array())
-    {
-        if (!$redis instanceof \Redis) {
-            throw new \InvalidArgumentException('Redis instance required');
-        }
+	/**
+	 * Constructor
+	 *
+	 * List of available options:
+	 *  * key_prefix: The key prefix [default: '']
+	 *
+	 * @param Redis   $redis    The redis instance
+	 * @param integer $lifetime Max lifetime in seconds to keep sessions stored.
+	 * @param array   $options  Options for the session handler
+	 *
+	 * @throws InvalidArgumentException When Redis instance not provided
+	 */
+	public function __construct( $redis, $lifetime, array $options = [] ) {
+		if ( !$redis instanceof Redis ) {
+			throw new InvalidArgumentException( 'Redis instance required' );
+		}
 
-        $this->redis = $redis;
-        $this->lifetime = $lifetime;
+		$this->redis    = $redis;
+		$this->lifetime = $lifetime;
 
-        if(!is_array($options)) $options = array();
-        $this->options = array_merge(array(
-            'key_prefix' => ''
-        ), $options);
-    }
+		if ( !is_array( $options ) ) {
+			$options = [];
+		}
+		$this->options = array_merge( [
+										  'key_prefix' => '',
+									  ],
+									  $options );
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function open($savePath, $sessionName)
-    {
-        return true;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public function open( $savePath, $sessionName ) {
+		return TRUE;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function read($sessionId)
-    {   
-        $key = $this->getKey($sessionId);
-        return (string) $this->redis->get($key);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public function read( $sessionId ) {
+		$key = $this->getKey( $sessionId );
 
-    /**
-     * {@inheritDoc}
-     */
-    public function write($sessionId, $data)
-    {
-        $key = $this->getKey($sessionId);
-        return $this->redis->setex($key, $this->lifetime, $data);
-    }
+		return (string)$this->redis->get( $key );
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function destroy($sessionId)
-    {
-        $key = $this->getKey($sessionId);
-        return 1 === $this->redis->delete($key);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public function write( $sessionId, $data ) {
+		$key = $this->getKey( $sessionId );
 
-    /**
-     * {@inheritDoc}
-     */
-    public function gc($lifetime)
-    {
-        /* Note: Redis will handle the expiration of keys with SETEX command
-         * See: http://redis.io/commands/setex
-         */
-        return true;
-    }
+		return $this->redis->setex( $key, $this->lifetime, $data );
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function close()
-    {
-        return true;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public function destroy( $sessionId ) {
+		$key = $this->getKey( $sessionId );
 
-    /**
-     * Get the redis key
-     * 
-     * @param string $sessionId session id
-     */
-    protected function getKey($sessionId)
-    {
-        if(is_string($this->options['key_prefix'])) {
-            return $this->options['key_prefix'].$sessionId;
-        }
-        return $sessionId;
-    }
+		return 1 === $this->redis->del( $key );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function gc( $lifetime ) {
+		/* Note: Redis will handle the expiration of keys with SETEX command
+		 * See: http://redis.io/commands/setex
+		 */
+		return TRUE;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function close() {
+		return TRUE;
+	}
+
+	/**
+	 * Get the redis key
+	 *
+	 * @param string $sessionId session id
+	 *
+	 * @return string
+	 */
+	protected function getKey( $sessionId ) {
+		if ( is_string( $this->options['key_prefix'] ) ) {
+			return $this->options['key_prefix'] . $sessionId;
+		}
+
+		return $sessionId;
+	}
 }
